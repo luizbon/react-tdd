1dfcc75f56a7fcb3f2162f2378b3f7022e85744e
Luiz Bon <luiz.bon@gmail.com>
2017-01-02 18:15:50 +1100

    07

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
-export const Breadcrumb = () => (<ul></ul>);
+export const Breadcrumb = () => (<ul><li></li></ul>);
 
 Breadcrumb.displayName = 'Breadcrumb';
 
