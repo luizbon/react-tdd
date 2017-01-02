6bed932b03442bb38a6641f673e5ff87d42c2564
Luiz Bon <luiz.bon@gmail.com>
2017-01-02 17:39:45 +1100

    01

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import { mount, shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const wrapper = mount(<App />);
-  expect(wrapper).toHaveLength(1);
+  const app = mount(<App />);
+  expect(app).toHaveLength(1);
+});
+
+describe('with breadcrumb', () => {
+  const app = shallow(<App />);
+  const breadcrumb = app.find('Breadcrumb');
+  expect(breadcrumb).toHaveLength(1);
 });
